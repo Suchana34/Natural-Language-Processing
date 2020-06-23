30c46ba1581d6f5e872cba43cf4dc6e44005a5fd
Suchana Chakrabarti <suchanachakraborty@gmail.com>
2020-06-24 02:59:21 +0530

    Update ner_nltk.py

--- ner_nltk.py
+++ ner_nltk.py
@@ -17,3 +17,25 @@ for sent in chunked_sentences:
     for chunk in sent:
         if hasattr(chunk, "label") and chunk.label() == "NE":
             print(chunk)
+            
+# Create the defaultdict: ner_categories
+ner_categories = defaultdict(int)
+
+# Create the nested for loop
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, 'label'):
+            ner_categories[chunk.label()] += 1
+            
+# Create a list from the dictionary keys for the chart labels: labels
+labels = list(ner_categories.keys())
+
+# Create a list of the values: values
+values = [ner_categories.get(v) for v in labels]
+
+# Create the pie chart showing different labels like person, organization, faculty, gpe and their percentage
+plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+
+# Display the chart
+plt.show()
+
